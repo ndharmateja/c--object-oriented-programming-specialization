902d69ae9c28a7f84417685d5540593c6dea6545
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-04-03 15:58:59 +0530

    change methods to static

--- project-merkel/CSVReader.h
+++ project-merkel/CSVReader.h
@@ -5,9 +5,9 @@
 class CSVReader
 {
 public:
-    std::vector<OrderBookEntry> read_csv(std::string filename);
+    static std::vector<OrderBookEntry> read_csv(std::string filename);
 
 private:
-    std::vector<std::string> tokenize(std::string csvLine, char separator);
-    OrderBookEntry convert_to_order_book_entry(std::vector<std::string> tokens);
+    static std::vector<std::string> tokenize(std::string csvLine, char separator);
+    static OrderBookEntry convert_to_order_book_entry(std::vector<std::string> tokens);
 };
