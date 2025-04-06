6a5abb1b37ffab339eae1c91328fea6c6e1ef01e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-04-06 16:54:27 +0530

    change to snake case

--- project-merkel/OrderBook.cpp
+++ project-merkel/OrderBook.cpp
@@ -4,14 +4,14 @@ OrderBook::OrderBook(std::string filename)
 {
     orders_ = CSVReader::read_csv(filename);
 }
-std::vector<std::string> OrderBook::getKnownProducts()
+std::vector<std::string> OrderBook::get_known_products()
 {
     std::vector<std::string> products;
     return products;
 }
-std::vector<OrderBookEntry> OrderBook::getOrders(OrderBookType type,
-                                                 std::string product,
-                                                 std::string timestamp)
+std::vector<OrderBookEntry> OrderBook::get_orders(OrderBookType type,
+                                                  std::string product,
+                                                  std::string timestamp)
 {
     std::vector<OrderBookEntry> orders;
     return orders;
--- project-merkel/OrderBook.h
+++ project-merkel/OrderBook.h
@@ -11,11 +11,11 @@ public:
     /** Constructor, reading a CSV file */
     OrderBook(std::string filename);
     /** return the list of known products */
-    std::vector<std::string> getKnownProducts();
+    std::vector<std::string> get_known_products();
     /** return vector of OrderBookEntries according to the filters */
-    std::vector<OrderBookEntry> getOrders(OrderBookType type,
-                                          std::string product,
-                                          std::string timestamp);
+    std::vector<OrderBookEntry> get_orders(OrderBookType type,
+                                           std::string product,
+                                           std::string timestamp);
 
 private:
     std::vector<OrderBookEntry> orders_;
