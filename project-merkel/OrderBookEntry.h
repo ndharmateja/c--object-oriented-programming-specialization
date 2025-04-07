45fe009ae2de15e4071aa3feb04247c0c3da9177
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-04-07 10:59:06 +0530

    add the static compare function

--- project-merkel/OrderBookEntry.h
+++ project-merkel/OrderBookEntry.h
@@ -19,6 +19,7 @@ public:
                    double amount);
 
     static OrderBookType string_to_order_book_type(std::string order_type);
+    static bool compare_by_timestamp(const OrderBookEntry &a, const OrderBookEntry &b) { return a.timestamp_ < b.timestamp_; }
 
     std::string timestamp_;
     std::string product_;
