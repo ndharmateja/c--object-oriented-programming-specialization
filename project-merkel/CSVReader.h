f0f583d54be4b959df8cb27e8db939ad5804fc81
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-04-07 09:57:08 +0530

    make tokenize function public and add new fn

--- project-merkel/CSVReader.h
+++ project-merkel/CSVReader.h
@@ -8,8 +8,14 @@ class CSVReader
 {
 public:
     static std::vector<OrderBookEntry> read_csv(std::string filename);
+    static std::vector<std::string> tokenize(std::string csvLine, char separator);
+    static OrderBookEntry convert_to_order_book_entry(
+        std::string timestamp,
+        std::string product,
+        OrderBookType type,
+        std::string price,
+        std::string amount);
 
 private:
-    static std::vector<std::string> tokenize(std::string csvLine, char separator);
     static OrderBookEntry convert_to_order_book_entry(std::vector<std::string> tokens);
 };
